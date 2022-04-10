7a2e46fd663170dbf5ac32eb11fd685542a81a08
unknown <cesare.polonara@gmail.com>
2022-04-10 20:13:17 +0200

    upated rtk auth service

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,97 +1,99 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import {
   retrieveAccessToken,
   retrieveRefreshToken,
   saveTokens,
   wipeTokensFromStorage,
-} from '../utils'
+} from "../utils";
 
 // Define a service using a base URL and expected endpoints
 export const authApi = createApi({
-  reducerPath: 'authApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'v1/' }),
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "/v1/",
+    prepareHeaders: (headers, { getState }) => {
+      const token = retrieveAccessToken();
+      headers.set("authorization", `Bearer ${token}`);
+      return headers;
+    },
+  }),
   endpoints: (builder) => ({
     checkAuth: builder.query({
       // note: an optional `queryFn` may be used in place of `query`
       query: () => {
-        const token = retrieveAccessToken()
-        const bearer = 'Bearer ' + token
         return {
-          url: 'auth/check-auth',
-          method: 'GET',
-          headers: {
-            Authorization: bearer,
-          },
-        }
+          url: "auth/check-auth",
+          method: "GET",
+        };
       },
-      providesTags: ['User'],
+      providesTags: ["User"],
     }),
     refreshTokens: builder.mutation({
       // note: an optional `queryFn` may be used in place of `query`
       query: () => {
-        const refreshToken = retrieveRefreshToken()
-        console.log('REFRESH TOKEN MUTATION', refreshToken)
+        const refreshToken = retrieveRefreshToken();
+        console.log("REFRESH TOKEN MUTATION", refreshToken);
         return {
           url: `auth/refresh-tokens`,
-          method: 'POST',
+          method: "POST",
           body: { refreshToken },
-        }
+        };
       },
       transformResponse: (response) => {
-        console.log('NEW TOKENS AFTER REFRESH MUTATION', response)
-        /* if (response.access && response.refresh) */ saveTokens(response)
-        return response
+        console.log("NEW TOKENS AFTER REFRESH MUTATION", response);
+        /* if (response.access && response.refresh) */ saveTokens(response);
+        return response;
       },
-      invalidatesTags: ['User'],
+      invalidatesTags: ["User"],
     }),
     register: builder.mutation({
       // note: an optional `queryFn` may be used in place of `query`
       query: ({ email, password, name }) => {
-        console.log(email, password, name)
+        console.log(email, password, name);
         return {
           url: `auth/register`,
-          method: 'POST',
+          method: "POST",
           body: { email, password, name },
-        }
+        };
       },
       transformResponse: (response) => {
-        if (response.tokens) saveTokens(response.tokens)
-        return response
+        if (response.tokens) saveTokens(response.tokens);
+        return response;
       },
-      invalidatesTags: ['User'],
+      invalidatesTags: ["User"],
     }),
     login: builder.mutation({
       // note: an optional `queryFn` may be used in place of `query`
       query: ({ email, password, name }) => ({
         url: `auth/login`,
-        method: 'POST',
+        method: "POST",
         body: { email, password },
       }),
       transformResponse: (response) => {
-        if (response.tokens) saveTokens(response.tokens)
-        return response
+        if (response.tokens) saveTokens(response.tokens);
+        return response;
       },
-      invalidatesTags: ['User'],
+      invalidatesTags: ["User"],
     }),
     logout: builder.mutation({
       // note: an optional `queryFn` may be used in place of `query`
       query: () => {
-        const refreshToken = retrieveRefreshToken()
+        const refreshToken = retrieveRefreshToken();
 
         return {
           url: `auth/logout`,
-          method: 'POST',
+          method: "POST",
           body: { refreshToken },
-        }
+        };
       },
       transformResponse: (response) => {
-        wipeTokensFromStorage()
-        return response
+        wipeTokensFromStorage();
+        return response;
       },
-      invalidatesTags: ['User'],
+      invalidatesTags: ["User"],
     }),
   }),
-})
+});
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
@@ -102,4 +104,4 @@ export const {
   useRegisterMutation,
   useRefreshTokensMutation,
   useLogoutMutation,
-} = authApi
+} = authApi;
